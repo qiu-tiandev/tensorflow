c8f7836aa21a617265abc7203510ce49747f2097
Alexander Belyaev <pifon@google.com>
2026-07-25 19:15:12 -0700

    [XLA:CPU] Create a tool to print xtile module after tiling for CPU.
    PiperOrigin-RevId: 954016559

--- third_party/xla/xla/backends/cpu/codegen/tiled/tools/fusion_to_xtile.cc
+++ third_party/xla/xla/backends/cpu/codegen/tiled/tools/fusion_to_xtile.cc
@@ -13,31 +13,43 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
 
 #include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "xla/tsl/platform/status_macros.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/raw_ostream.h"
-#include "mlir/IR/BuiltinOps.h"
+#include "xla/backends/cpu/codegen/fusion_compiler.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Pass/PassManager.h"
-#include "xla/backends/gpu/codegen/triton/triton_kernel_source.h"
-#include "xla/backends/gpu/codegen/triton/xtile_compiler.h"
-#include "xla/backends/gpu/target_config/target_config.h"
+#include "xla/backends/cpu/codegen/tiled/tiled_fusion_emitter.h"
+#include "xla/codegen/tiling/experimental/tiled_hlo.h"
+#include "xla/codegen/tiling/experimental/tiling_space.h"
+#include "xla/codegen/xtile/ir/xtile_dialect.h"
 #include "xla/debug_options_flags.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instructions.h"
+#include "xla/hlo/ir/hlo_print_options.h"
+#include "xla/hlo/utils/hlo_traversal.h"
 #include "xla/service/gpu/backend_configs.pb.h"
-#include "xla/service/gpu/gpu_device_info_for_tests.h"
 #include "xla/service/gpu/model/block_level_parameters.h"
-#include "xla/stream_executor/device_description.h"
+#include "xla/service/gpu/model/tiling_from_block_parameters.h"
+#include "xla/tools/hlo_decomposer.h"
 #include "xla/tools/hlo_module_loader.h"
+#include "xla/tsl/platform/logging.h"
 #include "xla/tsl/util/command_line_flags.h"
 #include "xla/util.h"
 #include "xla/xla.pb.h"
@@ -47,17 +59,24 @@ limitations under the License.
 namespace xla::gpu {
 namespace {
 
+using experimental::TiledHloComputation;
+using experimental::TilingSpace;
+
 absl::Status RealMain(absl::string_view input_file) {
   ASSIGN_OR_RETURN(std::unique_ptr<HloModule> hlo_module,
                    xla::LoadModuleFromFile(std::string(input_file)));
 
-  HloInstruction* fusion = hlo_module->entry_computation()->root_instruction();
-  if (!fusion->IsCustomFusion()) {
+  const HloInstruction& fusion =
+      *hlo_module->entry_computation()->root_instruction();
+  if (!fusion.IsCustomFusion()) {
     return absl::InvalidArgumentError("Instruction is not a custom fusion.");
   }
 
-  ASSIGN_OR_RETURN(auto gpu_config, fusion->backend_config<GpuBackendConfig>());
-  const HloFusionInstruction* fusion_instr = Cast<HloFusionInstruction>(fusion);
+  // After the new proto config for the XTile emitter is created, this should be
+  // updated.
+  ASSIGN_OR_RETURN(auto gpu_config, fusion.backend_config<GpuBackendConfig>());
+  const HloFusionInstruction* fusion_instr =
+      Cast<HloFusionInstruction>(&fusion);
   const FusionBackendConfig& backend_config =
       gpu_config.fusion_backend_config();
   if (!backend_config.has_block_level_fusion_config()) {
@@ -68,26 +87,32 @@ absl::Status RealMain(absl::string_view input_file) {
       BlockLevelParameters::FromBlockLevelFusionConfig(
           backend_config.block_level_fusion_config());
 
-  stream_executor::DeviceDescription device_info =
-      TestGpuDeviceInfo::RTXA6000DeviceInfo();
-  const std::string& target_config_filename =
-      hlo_module->config().debug_options().xla_gpu_target_config_filename();
-  if (!target_config_filename.empty()) {
-    ASSIGN_OR_RETURN(auto target_config,
-                     GetTargetConfigFromFile(target_config_filename));
-    device_info = target_config.device_description;
-  }
+  auto fusion_adaptor = HloFusionAdaptor::ForInstruction(&fusion);
 
-  mlir::MLIRContext mlir_context;
-  // CreateTritonModule creates an xtile dialect module that
-  // CreateTritonXlaPipeline() will lower to TTIR.
-  absl::StatusOr<TritonKernelSource> triton_source =
-      CreateTritonModule("triton_fn", *fusion_instr, device_info,
-                         block_level_parameters, mlir_context);
-  if (!triton_source.ok()) {
-    return triton_source.status();
+  auto mlir_context = cpu::FusionCompiler::CreateContext();
+  ASSIGN_OR_RETURN(std::unique_ptr<TilingSpace> tiling_space,
+                   TilingSpace::Create(*fusion_adaptor, mlir_context.get()));
+
+  VLOG(3) << "fusion instruction: " << fusion.ToString() << "\n";
+  VLOG(3) << "tiling space: " << tiling_space->ToString();
+  if (VLOG_IS_ON(4)) {
+    XLA_VLOG_LINES(
+        4, absl::StrCat("HLO module to reproduce:\n",
+                        ExtractInstructionIntoNewModule(fusion)->ToString(
+                            HloPrintOptions::ShortParsable())));
   }
-  triton_source->module().print(llvm::outs());
+  ASSIGN_OR_RETURN(
+      llvm::SmallVector<int64_t> tile_sizes,
+      GetTilingSpaceConcreteSizes(*tiling_space, block_level_parameters));
+
+  cpu::TiledEmissionResult result = cpu::EmitTiledFusionKernel(
+      *mlir_context, *fusion_instr, /*buffer_assignment=*/nullptr,
+      "wrapped_fusion", /*num_work_groups=*/block_level_parameters.num_ctas,
+      tile_sizes);
+  if (!result.kernel.ok()) {
+    return result.kernel.status();
+  }
+  result.kernel->source().module().print(llvm::outs());
   return absl::OkStatus();
 }
 
